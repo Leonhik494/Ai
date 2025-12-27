8a48e2cdc0cddff30a51422e9a21e981b3319718
Leonhik494 <leonhik02032014@gmail.com>
2025-12-27 17:22:19 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2158,7 +2158,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
